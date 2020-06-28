c8465d6e62a4e333c983a22e6473aba9bff71dc7
tatanalosinina <tatana@localhost>
2020-06-28 10:24:26 +0300

    fix

--- src/test/java/ru/netology/StaticTest.java
+++ src/test/java/ru/netology/StaticTest.java
@@ -1,14 +1,19 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StaticTest {
-    @org.junit.jupiter.api.Test
+    @Test
     void findMax() {
         Static service = new Static();
-        long[] incomesInBillions = {12, 15, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 15;
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
         long actual = service.findMax(incomesInBillions);
+
         assertEquals(expected, actual);
     }
   //  @org.junit.jupiter.api.Test
